0943c5e111b235ca15021387e6efc75bbf127fb3
Pratik346 <pratikkumarmohanty9@gmail.com>
2026-04-22 16:52:15 +0530

    added port

--- server/server.js
+++ server/server.js
@@ -17,4 +17,4 @@ app.get("/",(req,res)=>{
     res.send("FlashCard Server started")
 });
 const PORT=process.env.PORT || 5000;
-app.listen(port,()=>console.log(`Server running on port ${PORT}`));
+app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
